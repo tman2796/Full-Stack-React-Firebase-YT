1e0b1f0797d8d1bbe37f505550c0351b92b3fe08
Travis Simmons <tsimmons6@ggc.edu>
2020-03-23 12:53:51 -0400

    Created POST and GET requests for the socialApe social media web application

---
+++ dbschema.js
@@ -0,0 +1,9 @@
+let db = {
+    screams: [{
+        userHandle: 'user',
+        body: "this is the scream body",
+        createdAt: '2020-03-23T16:49:09.281Z',
+        likeCount: 5,
+        commentCount: 2
+    }]
+}
---
+++ functions/index.js
@@ -0,0 +1,61 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+
+var serviceAccount = require("./ServiceAccountKey/socialape-41891-firebase-adminsdk-g4grm-c7847520a1.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://socialape-41891.firebaseio.com"
+});
+
+const express = require('express');
+const app = express();
+
+app.get('/screams', (req, res) => {
+    admin
+        .firestore()
+        .collection('screams')
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then((data) => {
+            let screams = [];
+            data.forEach((doc) => {
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                });
+            });
+            return res.json(screams);
+        })
+        .catch((err) => console.error(err));
+})
+
+app.post('/scream', (req, res) => {
+    const newScream = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: new Date().toISOString()
+    };
+
+    admin
+        .firestore()
+        .collection('screams')
+        .add(newScream)
+        .then((doc) => {
+            res.json({
+                message: `document ${doc.id} created successfully`
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: 'something went wrong'
+            })
+            console.error(err)
+        })
+});
+
+// https://baseurl.com/api/screams -- good practice
+
+exports.api = functions.https.onRequest(app);

22785512f98dc5ca5e0cce42848579881652eab6
Travis Simmons <tsimmons6@ggc.edu>
2020-03-23 14:37:47 -0400

    added login authorization and more

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,31 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin')
+const app = require('express')();
 
-var serviceAccount = require("./socialape-41891-firebase-adminsdk-g4grm-c7847520a1.json");
-
+var serviceAccount = require("../../../Firebase-FullStack-Application-ServiceKey/socialape-41891-firebase-adminsdk-g4grm-c7847520a1.json");
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://socialape-41891.firebaseio.com"
 });
 
-const express = require('express');
-const app = express();
+const firebaseConfig = {
+    apiKey: "AIzaSyACWQMgJdKZpQHnUHVtCmq6nCiLU4TMTDQ",
+    authDomain: "socialape-41891.firebaseapp.com",
+    databaseURL: "https://socialape-41891.firebaseio.com",
+    projectId: "socialape-41891",
+    storageBucket: "socialape-41891.appspot.com",
+    messagingSenderId: "461007202384",
+    appId: "1:461007202384:web:c13563e4f946c208ca4955",
+    measurementId: "G-JDL7N23DT4"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
 
 app.get('/screams', (req, res) => {
-    admin
-        .firestore()
+    db
         .collection('screams')
         .orderBy('createdAt', 'desc')
         .get()
@@ -32,15 +44,53 @@ app.get('/screams', (req, res) => {
         .catch((err) => console.error(err));
 })
 
-app.post('/scream', (req, res) => {
+
+const FBAuth = (req, res, next) => {
+    let idToken;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found')
+        return res.status(403).json({
+            error: 'Unauthorized'
+        });
+    }
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+            console.log(decodedToken)
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then(data => {
+            req.user.handle = data.docs[0].data().handle;
+            return next();
+        })
+        .catch((err) => {
+            console.error('Error while verifying token ', err);
+            return res.status(403).json(err);
+        })
+}
+
+// Post one scream
+app.post('/scream', FBAuth, (req, res) => {
+
+    if (req.body.body.trim() === '') {
+        return res.status(400).json({
+            body: 'Body must not be empty'
+        });
+    }
+
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString()
     };
 
-    admin
-        .firestore()
+    db
         .collection('screams')
         .add(newScream)
         .then((doc) => {
@@ -57,5 +107,126 @@ app.post('/scream', (req, res) => {
 });
 
 // https://baseurl.com/api/screams -- good practice
+
+
+const isEmpty = (string) => {
+    if (string.trim() === '') return true;
+    else return false;
+}
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9 {1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.match(regEx)) return true;
+    else return false;
+}
+
+// Signup route
+
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    };
+
+    let errors = {};
+
+
+    if (isEmpty(newUser.email)) {
+        errors.email = 'Must not be empty'
+    } else if (!isEmail(newUser.email)) {
+        errors.email = 'Must be a valid email address'
+    }
+
+    if (isEmpty(newUser.password)) errors.password = 'Must not be empty'
+    if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+    if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+    // Make sure errors object is empty
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors)
+    // TODO validate data
+
+    let token, userId;
+
+    db.doc(`/users/${newUser.handle}`).get()
+        .then(doc => {
+            if (doc.exists) {
+                return res.status(400).json({
+                    handle: 'this handle is already taken'
+                })
+            } else {
+                return firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+            }
+        })
+        .then(data => { //User is created down here
+            userId = data.user.uid;
+            return data.user.getIdToken();
+        })
+        .then(idToken => {
+            token = idToken;
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId
+            }
+            // Persist these into a collection
+            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+        })
+        .then(() => {
+            return res.status(201).json({
+                token
+            });
+        })
+        .catch(err => {
+            console.error(err);
+            if (err.code === 'auth/email-already-in-use') {
+                return res.status(400).json({
+                    email: 'Email is already in use'
+                })
+            } else {
+                return res.status(500).json({
+                    error: err.code
+                });
+            }
+        })
+});
+
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    let errors = {}
+
+    if (isEmpty(user.email)) errors.email = 'Must not be empty'
+    if (isEmpty(user.password)) errors.password = 'Must not be empty'
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+            return data.user.getIdToken();
+        })
+        .then(token => {
+            return res.json({
+                token
+            });
+        })
+        .catch(err => {
+            console.error(err);
+            if (err.code === 'auth/wrong-password') {
+                res.status(403).json({
+                    general: 'Wrong credentials, please try again'
+                });
+            } else return res.status(500).json({
+                error: err.code
+            });
+        });
+
+});
 
 exports.api = functions.https.onRequest(app);

20baf89b163c3e725167f9005bddddd5133c7353
Travis Simmons <tsimmons6@ggc.edu>
2020-03-24 12:27:09 -0400

    Added more details to user in schema template

--- dbschema.js
+++ dbschema.js
@@ -1,9 +1,19 @@
 let db = {
+    users: [{
+        userId: 'dh23ggj5h32g543j5gf43',
+        email: 'user@email.com',
+        handle: 'user',
+        createdAt: '2020-03-23T16:49:09.281Z',
+        imageUrl: 'image/dsfsdfdsg/asdasdas',
+        bio: 'Hello my name is user and im a user',
+        website: 'https://user.com',
+        location: 'Georgia, USA'
+    }],
     screams: [{
         userHandle: 'user',
         body: "this is the scream body",
         createdAt: '2020-03-23T16:49:09.281Z',
         likeCount: 5,
-        commentCount: 2
+        commentCount: 3
     }]
-}
+};

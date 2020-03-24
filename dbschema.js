8f3b656b3ad7bb956a2327d619f55ab561a25bbd
Travis Simmons <tsimmons6@ggc.edu>
2020-03-24 14:54:55 -0400

    added more sample data to schema

--- dbschema.js
+++ dbschema.js
@@ -15,5 +15,34 @@ let db = {
         createdAt: '2020-03-23T16:49:09.281Z',
         likeCount: 5,
         commentCount: 3
+    }],
+    comments: [{
+        userHandle: 'user',
+        screamId: 'sdffsdgfdssdfafd',
+        body: 'this is the body of a comment on a scream',
+        createdAt: '2020-03-23T16:49:09.281Z',
     }]
 };
+
+const userDetails = {
+    //Redux data
+    credentials: {
+        userId: 'N456JDSF4564ASD3165B',
+        email: 'user@email.com',
+        handle: 'user',
+        createdAt: '2020-03-23T16:49:09.281Z',
+        imageUrl: 'image/dsfsdfdsg/asdasdas/asddgsgf',
+        bio: 'Hello my name is user, nice to meet you',
+        website: 'https://user.com',
+        location: 'Georgia, USA'
+    },
+    likes: [{
+            userHandle: 'user',
+            screamId: 'sdgfdfhgfhsdgsad'
+        },
+        {
+            userHandle: 'user',
+            screamId: '654asdsdgsfdsafasd'
+        }
+    ]
+}

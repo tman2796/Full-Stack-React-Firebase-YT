6af247d00ae008f1a35941f14f13e362502317be
Travis Simmons <tsimmons6@ggc.edu>
2020-03-24 14:54:24 -0400

    Comment on screams

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -11,10 +11,11 @@ firebase.initializeApp(config)
 
 const {
     validateSignupData,
-    validateLoginData
+    validateLoginData,
+    reduceUserDetails
 } = require('../util/validators');
 
-
+// signs user up for account
 exports.signup = (req, res) => {
     const newUser = {
         email: req.body.email,
@@ -81,7 +82,7 @@ exports.signup = (req, res) => {
             }
         })
 }
-
+// Logs user in
 exports.login = (req, res) => {
     const user = {
         email: req.body.email,
@@ -117,8 +118,50 @@ exports.login = (req, res) => {
 
 }
 
-// 
+// Add user details
 
+exports.addUserDetails = (req, res) => {
+    let userDetails = reduceUserDetails(req.body);
+
+    db.doc(`/users/${req.user.handle}`).update(userDetails)
+        .then(() => {
+            return res.json({
+                message: 'Details added successfully'
+            });
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({
+                error: err.code
+            });
+        })
+
+}
+//Get own user details
+exports.getAuthenticatedUser = (req, res) => {
+    let userData = {}; // response data for user
+    db.doc(`/users/${req.user.handle}`).get()
+        .then(doc => {
+            if (doc.exists) {
+                userData.credentials = doc.data();
+                return db.collection('likes').where('userHandle', '==', req.user.handle).get();
+            }
+        })
+        .then(data => {
+            userData.likes = [];
+            data.forEach(doc => {
+                userData.likes.push(doc.data());
+            });
+            return res.json(userData);
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({
+                error: err.code
+            });
+        })
+}
+// upload profile image to user
 exports.uploadImage = (req, res) => {
     const Busboy = require('busboy');
     const path = require('path');
--- functions/index.js
+++ functions/index.js
@@ -6,25 +6,36 @@ const FBAuth = require('./util/fbAuth');
 
 const {
     getAllScreams,
-    postOneScream
+    postOneScream,
+    getScream,
+    commentOnScream
 } = require('./handlers/screams')
 const {
     signup,
     login,
     uploadImage,
-    addUserDetails
+    addUserDetails,
+    getAuthenticatedUser
 } = require('./handlers/users')
-
 
 //Scream routes
 app.get('/screams', getAllScreams)
 app.post('/scream', FBAuth, postOneScream);
-app.post('/user', FBAuth, addUserDetails)
+app.get('/scream/:screamId', getScream);
+
+// TODO write routes
+// Delete scream
+// Like a scream
+// unlike a scream
+// comment on scream
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream)
 
 // users routes
 app.post('/signup', signup);
 app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
+app.get('/user', FBAuth, getAuthenticatedUser)
+app.post('/user', FBAuth, addUserDetails)
 // https://baseurl.com/api/screams -- good practice
 
 exports.api = functions.https.onRequest(app);

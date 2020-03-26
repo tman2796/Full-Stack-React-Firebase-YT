424ddd772fb81acc2c001381ed6d95d4234d5de2
Travis Simmons <tsimmons6@ggc.edu>
2020-03-26 09:06:21 -0400

    fixed service key placement issue for deploying project to test

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,10 @@ const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
 
+// const {
+//     db
+// } = require('./util/admin');
+
 const {
     getAllScreams,
     postOneScream,
@@ -45,3 +49,72 @@ app.post('/user', FBAuth, addUserDetails);
 // https://baseurl.com/api/screams -- good practice
 
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions
+    .region('us-central1')
+    .firestore.document('likes/{id}')
+    .onCreate((snapshot) => {
+        db.doc(`/screams/${snapshot.data().screamId}`)
+            .get()
+            .then((doc) => {
+                if (doc.exists) {
+                    return db.doc(`/notifications/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: 'like',
+                        read: false,
+                        screamId: doc.id
+                    });
+                }
+            })
+            .then(() => {
+                return;
+            })
+            .catch((err) => {
+                console.error(err)
+                return;
+            });
+    })
+
+exports.deleteNotificationOnUnlike = functions
+    .firestore
+    .document('likes/{id}')
+    .onDelete((snapshot) => {
+        db.doc(`/notifications/${snapshot.id}`)
+            .delete()
+            .then(() => {
+                return;
+            })
+            .catch(err => {
+                console.error(err);
+                return;
+            })
+    })
+
+exports.createNotificationOnComment = functions
+    .firestore
+    .document('comments/{id}')
+    .onCreate((snapshot) => {
+        db.doc(`/screams/${snapshot.data().screamId}`)
+            .get()
+            .then(doc => {
+                if (doc.exists) {
+                    return db.doc(`/notifications/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: 'comment',
+                        read: false,
+                        screamId: doc.id
+                    });
+                }
+            })
+            .then(() => {
+                return;
+            })
+            .catch(err => {
+                console.error(err)
+                return;
+            });
+    })
--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,7 +1,8 @@
 const admin = require('firebase-admin')
 
-var serviceAccount = require("../../../../Firebase-FullStack-Application-ServiceKey/socialape-41891-firebase-adminsdk-g4grm-c7847520a1.json");
+const serviceAccount = require('../socialape-41891-firebase-adminsdk-g4grm-c7847520a1.json')
 admin.initializeApp({
+    // credential: admin.credential.applicationDefault(),
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://socialape-41891.firebaseio.com",
     storageBucket: "socialape-41891.appspot.com"
@@ -9,4 +10,7 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-module.exports = { admin, db };
+module.exports = {
+    admin,
+    db
+};
